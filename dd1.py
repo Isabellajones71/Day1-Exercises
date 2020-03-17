f5355d0299c4d7eac989a0c62e67e75ea4a6049c
Isabella Jones <isabellajones71@gmail.com>
2020-03-17 14:31:22 +0000

    First commit

---
+++ dd1.py
@@ -0,0 +1,17 @@
+# print("Hello")
+# a= 10
+# b = 20
+# if (a>b):
+#     print("{} is bigger than {}".format(a,b))
+# else:
+#     print("{}  is bigger than {}".format(b,a))
+
+# list_data = [1, 2, 3, 4, 5]
+# #
+# # for m in list_data:
+# #     print(m * 2)
+
+
+#Declare a list of 10 numbers
+# have a check condition like if a<15
+#If the condition is satisfied write it to another list and print the list
